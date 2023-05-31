352affe7fe240c8374e741dfd57feff412a97717
Eduardo Sánchez <eduardosr1599@gmail.com>
2023-05-31 18:16:38 -0400

    Style Signup component

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,22 +1,27 @@
 import React from 'react';
+import '../styles/signup.css';
 
 export default function Signup({ visible, closeModal }) {
     return (
         <div>
             {visible && (
-                <form>
-                    <button type="button" onClick={closeModal}>
-                        X
-                    </button>
-                    <h2>Sign up</h2>
-                    <p>It&apos;s quick and easy</p>
-                    <input type="text" placeholder="First name" />
-                    <input type="text" placeholder="Surname" />
-                    <input type="email" placeholder="Email" />
-                    <input type="password" placeholder="New password" />
-                    <input type="password" placeholder="Confirm password" />
-                    <button type="button">Sign up</button>
-                </form>
+                <div className="overlay">
+                    <form>
+                        <button type="button" onClick={closeModal}>
+                            X
+                        </button>
+                        <h2>Sign up</h2>
+                        <p>It&apos;s quick and easy</p>
+                        <div className="name">
+                            <input type="text" placeholder="First name" />
+                            <input type="text" placeholder="Surname" />
+                        </div>
+                        <input type="email" placeholder="Email" />
+                        <input type="password" placeholder="New password" />
+                        <input type="password" placeholder="Confirm password" />
+                        <button type="button">Sign up</button>
+                    </form>
+                </div>
             )}
         </div>
     );
