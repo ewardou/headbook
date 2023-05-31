5799ef54c8b7f7c6d75cf7082c31c1bd9ca8c9c0
Eduardo Sánchez <eduardosr1599@gmail.com>
2023-05-31 14:20:52 -0400

    Make Signup component render conditionally

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,18 +1,23 @@
 import React from 'react';
 
-export default function Signup() {
+export default function Signup({ visible, closeModal }) {
     return (
         <div>
-            <h2>Sign up</h2>
-            <p>It&apos;s quick and easy</p>
-            <form>
-                <input type="text" placeholder="First name" />
-                <input type="text" placeholder="Surname" />
-                <input type="email" placeholder="Email" />
-                <input type="password" placeholder="New password" />
-                <input type="password" placeholder="Confirm password" />
-                <button type="button">Sign up</button>
-            </form>
+            {visible && (
+                <form>
+                    <button type="button" onClick={closeModal}>
+                        X
+                    </button>
+                    <h2>Sign up</h2>
+                    <p>It&apos;s quick and easy</p>
+                    <input type="text" placeholder="First name" />
+                    <input type="text" placeholder="Surname" />
+                    <input type="email" placeholder="Email" />
+                    <input type="password" placeholder="New password" />
+                    <input type="password" placeholder="Confirm password" />
+                    <button type="button">Sign up</button>
+                </form>
+            )}
         </div>
     );
 }
