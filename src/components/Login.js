381519d46553a5251622017918aded133af4a268
Eduardo Sánchez <eduardosr1599@gmail.com>
2023-05-30 22:22:26 -0400

    Add styling to login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,19 @@
 import React, { useState } from 'react';
+import '../styles/login.css';
 
 export default function Login() {
     const [email, setEmail] = useState('');
     const [pwd, setPwd] = useState('');
 
     return (
-        <div>
-            <h1>headbook</h1>
-            <p>
-                Headbook helps you connect and share with the people in your
-                life.
-            </p>
+        <div className="login">
+            <div>
+                <h1>headbook</h1>
+                <p>
+                    Headbook helps you connect and share with the people in your
+                    life.
+                </p>
+            </div>
             <form>
                 <input
                     type="email"
@@ -25,7 +28,9 @@ export default function Login() {
                     onChange={(e) => setPwd(e.target.value)}
                 />
                 <button type="submit">Log in</button>
-                <button type="button">Create new account</button>
+                <a href="/sign-up">Create new account</a>
+                <button type="button">Log in with Facebook</button>
+                <button type="button">Continue as guest</button>
             </form>
         </div>
     );
