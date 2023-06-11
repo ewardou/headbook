049c1a1f565ce6a4bb40e4e6079a8fa1c7cf66a3
Eduardo Sánchez <eduardosr1599@gmail.com>
2023-06-11 13:38:24 -0400

    Make the inputs in signup form controlled

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,25 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/signup.css';
 
 export default function Signup({ visible, closeModal }) {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [passwordConfirm, setConfirmPassword] = useState('');
+    const [message, setMessage] = useState('');
+    const [disabled, setDisabled] = useState(false);
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        if (password !== passwordConfirm) {
+            setMessage('Password are not equal');
+        } else {
+            setMessage('Form submitted');
+            setDisabled(true);
+        }
+    };
     return (
         <div>
             {visible && (
                 <div className="overlay">
-                    <form>
+                    <form onSubmit={onSubmit}>
                         <button type="button" onClick={closeModal}>
                             X
                         </button>
                         <h2>Sign up</h2>
                         <p>It&apos;s quick and easy</p>
                         <div className="name">
-                            <input type="text" placeholder="First name" />
-                            <input type="text" placeholder="Surname" />
+                            <input
+                                type="text"
+                                placeholder="First name"
+                                value={firstName}
+                                onChange={(e) => setFirstName(e.target.value)}
+                                required
+                            />
+                            <input
+                                type="text"
+                                placeholder="Last name"
+                                value={lastName}
+                                onChange={(e) => setLastName(e.target.value)}
+                                required
+                            />
                         </div>
-                        <input type="email" placeholder="Email" />
-                        <input type="password" placeholder="New password" />
-                        <input type="password" placeholder="Confirm password" />
-                        <button type="button">Sign up</button>
+                        <input
+                            type="email"
+                            placeholder="Email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            required
+                        />
+                        <input
+                            type="password"
+                            placeholder="New password"
+                            minLength={8}
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            required
+                        />
+                        <input
+                            type="password"
+                            placeholder="Confirm password"
+                            minLength={8}
+                            value={passwordConfirm}
+                            onChange={(e) => setConfirmPassword(e.target.value)}
+                            required
+                        />
+                        <p>{message}</p>
+                        <button type="submit" disabled={disabled}>
+                            Sign up
+                        </button>
                     </form>
                 </div>
             )}
