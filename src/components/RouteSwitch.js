111a2e343e4f43046d0ae490554c6c76844dc90a
Eduardo Sánchez <eduardosr1599@gmail.com>
2023-06-15 17:25:45 -0400

    Update RouteSwitch

--- src/components/RouteSwitch.js
+++ src/components/RouteSwitch.js
@@ -2,12 +2,17 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './Login';
 import Home from './Home';
+import People from './People';
+import Posts from './Posts';
 
 function RouteSwitch() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Home />} />
+                <Route path="/" element={<Home />}>
+                    <Route path="" element={<Posts />} />
+                    <Route path="people" element={<People />} />
+                </Route>
                 <Route path="/login" element={<Login />} />
             </Routes>
         </BrowserRouter>
