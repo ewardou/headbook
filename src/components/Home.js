557b5f31b52b5ceb952f4b1fe2b98311256be461
Eduardo Sánchez <eduardosr1599@gmail.com>
2023-06-15 17:21:43 -0400

    Modify Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,35 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { Outlet, Link } from 'react-router-dom';
 
 function Home() {
-    const [permission, setPermission] = useState(false);
-    async function getPermission() {
-        try {
-            const response = await fetch(
-                'https://headbook-7930.onrender.com/protected',
-                {
-                    method: 'GET',
-                    headers: {
-                        Authorization: `Bearer ${localStorage.getItem(
-                            'token'
-                        )}`,
-                    },
-                }
-            );
-            if (response.ok) {
-                setPermission(true);
-            } else {
-                throw new Error('Log in to access this route');
-            }
-        } catch (e) {
-            window.location.assign('/login');
-        }
-    }
-
-    useEffect(() => {
-        getPermission();
-    }, []);
-
-    return permission ? <h1>Protected route</h1> : <h1>Loading</h1>;
+    return (
+        <div>
+            <header>
+                <Link to="/">Posts</Link>
+                <Link to="people">People</Link>
+            </header>
+            <Outlet />
+        </div>
+    );
 }
 
 export default Home;
