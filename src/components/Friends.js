0ba923d8d5f1fd2acf1a07511ed7e6679efae2ad
Eduardo Sánchez <eduardosr1599@gmail.com>
2023-06-20 18:56:06 -0400

    Create Friends component

---
+++ src/components/Friends.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useOutletContext } from 'react-router-dom';
+
+export default function Friends() {
+    const { user, content } = useOutletContext();
+    const array = content.filter((el) => user.friends.includes(el._id));
+    const friends = array.map((el) => (
+        <div key={el._id}>
+            <img src={el.profilePicture} alt="" />
+            <p>
+                {el.firstName} {el.lastName}
+            </p>
+        </div>
+    ));
+    return <div>{friends}</div>;
+}
