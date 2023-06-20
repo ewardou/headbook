c73b97211eb2e5fadde97ba7ed889593ce949457
Eduardo Sánchez <eduardosr1599@gmail.com>
2023-06-20 18:57:04 -0400

    Create Suggestions component

---
+++ src/components/Suggestions.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useOutletContext } from 'react-router-dom';
+import RequestButton from './RequestButton';
+
+export default function Suggestions() {
+    const { user, content } = useOutletContext();
+    const array = content.filter((el) => !user.friends.includes(el._id));
+
+    return (
+        <div className="people">
+            {array.map((el) => (
+                <div key={el._id}>
+                    <img src={el.profilePicture} alt="" />
+                    <p>
+                        {el.firstName} {el.lastName}
+                    </p>
+                    <RequestButton id={el._id} requestsArray={el.requests} />
+                </div>
+            ))}
+        </div>
+    );
+}
