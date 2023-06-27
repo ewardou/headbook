ec259f5eb51a92b48baea625a2b9da9775be4c75
Eduardo Sánchez <eduardosr1599@gmail.com>
2023-06-26 22:16:28 -0400

    Replace previous code block with PostCards in Posts

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,9 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import ReactLoading from 'react-loading';
-import { DateTime } from 'luxon';
+import PostCards from './PostCards';
 import NewPost from './NewPost';
-import LikeButton from './LikeButton';
-import Comments from './Comments';
 import '../styles/post.css';
 
 export default function Posts() {
@@ -29,10 +27,6 @@ export default function Posts() {
         }
     }
 
-    function formatDate(date) {
-        return DateTime.fromISO(date).toLocaleString(DateTime.DATETIME_MED);
-    }
-
     useEffect(() => {
         getPosts();
     }, []);
@@ -41,30 +35,7 @@ export default function Posts() {
         return (
             <div className="posts">
                 <NewPost />
-                <div>
-                    {posts.map((post) => (
-                        <div key={post._id} className="post-card">
-                            <div>
-                                <img src={post.author.profilePicture} alt="" />
-                                <div>
-                                    <p>
-                                        {post.author.firstName}{' '}
-                                        {post.author.lastName}
-                                    </p>
-                                    <p>{formatDate(post.date)}</p>
-                                </div>
-                            </div>
-                            <p style={{ margin: '10px 0px' }}>{post.content}</p>
-                            <div className="buttons">
-                                <LikeButton
-                                    likes={post.likes}
-                                    postID={post._id}
-                                />
-                                <Comments postID={post._id} />
-                            </div>
-                        </div>
-                    ))}
-                </div>
+                <PostCards posts={posts} />
             </div>
         );
     }
