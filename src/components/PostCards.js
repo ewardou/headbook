c94fd9db64b766adb491fd11f938a37e5a22a56c
Eduardo Sánchez <eduardosr1599@gmail.com>
2023-06-26 22:13:47 -0400

    Create PostCards component

---
+++ src/components/PostCards.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { DateTime } from 'luxon';
+import LikeButton from './LikeButton';
+import Comments from './Comments';
+
+export default function PostCards({ posts }) {
+    function formatDate(date) {
+        return DateTime.fromISO(date).toLocaleString(DateTime.DATETIME_MED);
+    }
+
+    return (
+        <div>
+            {posts.map((post) => (
+                <div key={post._id} className="post-card">
+                    <div>
+                        <img src={post.author.profilePicture} alt="" />
+                        <div>
+                            <p>
+                                {post.author.firstName} {post.author.lastName}
+                            </p>
+                            <p>{formatDate(post.date)}</p>
+                        </div>
+                    </div>
+                    <p style={{ margin: '10px 0px' }}>{post.content}</p>
+                    <div className="buttons">
+                        <LikeButton likes={post.likes} postID={post._id} />
+                        <Comments postID={post._id} />
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+}
