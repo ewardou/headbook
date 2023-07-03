13e5406b9dc1791247325518c09aa829819c78a2
Eduardo Sánchez <eduardosr1599@gmail.com>
2023-07-02 21:02:09 -0400

    Create ProfileModal

---
+++ src/components/ProfileModal.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+export default function ProfileModal({ user, profilePicture }) {
+    const [visible, setVisible] = useState(false);
+
+    function showModal() {
+        setVisible((prevState) => !prevState);
+    }
+
+    function logOut() {
+        localStorage.removeItem('token');
+        window.location.assign('/login');
+    }
+
+    return (
+        <div className="profile-modal">
+            <button type="button" onClick={showModal}>
+                <img src={profilePicture} alt="My profile" />
+            </button>
+            <div className={visible ? 'visible' : ''}>
+                <Link to={user ? `profiles/${user._id}` : null}>
+                    My profile
+                </Link>
+                <button type="button" onClick={logOut}>
+                    Log out
+                </button>
+            </div>
+        </div>
+    );
+}
