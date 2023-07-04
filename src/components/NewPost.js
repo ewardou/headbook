309409f54cd43e140a958cad28f68f625e5a9404
Eduardo Sánchez <eduardosr1599@gmail.com>
2023-07-04 13:42:25 -0400

    Add disabled state to NewPost component

--- src/components/NewPost.js
+++ src/components/NewPost.js
@@ -1,14 +1,17 @@
 import React, { useState } from 'react';
 import { useOutletContext } from 'react-router-dom';
+import ReactLoading from 'react-loading';
 import ImageInput, { uploadPic } from './ImageInput';
 import '../styles/new-post.css';
 
 export default function NewPost({ getPosts }) {
     const { user } = useOutletContext();
     const [content, setContent] = useState('');
+    const [disabled, setDisabled] = useState(false);
 
     async function createPost() {
         try {
+            setDisabled(true);
             const image = await uploadPic();
             const response = await fetch(
                 'https://headbook-7930.onrender.com/new-post',
@@ -27,9 +30,11 @@ export default function NewPost({ getPosts }) {
                 throw response;
             }
             setContent('');
+            setDisabled(false);
             getPosts();
         } catch (e) {
             console.error(e);
+            setDisabled(false);
         }
     }
 
@@ -57,7 +62,19 @@ export default function NewPost({ getPosts }) {
                     style={{ display: 'flex', justifyContent: 'space-between' }}
                 >
                     <ImageInput />
-                    <button type="submit">Post</button>
+                    <button type="submit" disabled={disabled}>
+                        {disabled ? (
+                            <ReactLoading
+                                type="spin"
+                                color="#722F37"
+                                className="loading"
+                                height={25}
+                                width={25}
+                            />
+                        ) : (
+                            'Post'
+                        )}
+                    </button>
                 </div>
             </form>
         </div>
